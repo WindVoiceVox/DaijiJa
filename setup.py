c919a5bbca8b7907eb52771edfb186ae1021b4e9
WindVoiceVox <windvoice.vox@gmail.com>
2023-01-02 13:54:36 +0900

    ライセンス表記を追加

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ NAME = 'DaijiJa'
 setup(
     name=NAME,
     version='0.1',
+    author='WindVoiceVox',
+    url='https://github.com/WindVoiceVox/DaijiJa',
     description='Japanese Daiji library',
+    license='MIT License',
     packages=find_packages()
 )
